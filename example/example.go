da958e1c03c117363c88accd348b078f8f665751
aaronwinter <aaron.ounn@gmail.com>
2018-04-01 01:24:51 +0200

    Describe usage for verbose and garbage collection optim. flags

---
+++ example/example.go
@@ -0,0 +1,20 @@
+package main
+
+type Something struct {
+	A string
+	B string
+	C string
+	J bool
+
+	D string
+	E string
+	F string
+	K bool
+
+	G string
+	H string
+	I string
+	L bool
+
+	M *int
+}
